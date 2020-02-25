f913f3e7f08bb2fed29829b2e434b048a426ed30
grislux55 <jebchou.hg@gmail.com>
2020-02-25 18:47:25 +0800

    ramdisk: Remove and cleanup
    The in-tree ramdisk and kernel image builder is useless for us.
    Remove it.
    
    Signed-off-by: grislux55 <jebchou.hg@gmail.com>

--- build_master.sh
+++ build_master.sh
@@ -2,20 +2,22 @@
 
 if [[ "${1}" != "skip" ]] ; then
 	./build_clean.sh
-	./build_kernel.sh stock "$@" || exit 1
+	./build_kernel.sh "$@" || exit 1
 fi
 
-VERSION="$(cat version)-$(date +%F | sed s@-@@g)"
+VERSION="$(cat version)"
 
-if [ -e boot.img ] ; then
-	rm arter97-kernel-$VERSION.zip 2>/dev/null
-	cp boot.img arter97-kernel-$VERSION.img
+if [ -e arch/arm64/boot/Image.gz ] ; then
+	echo "Packing Kernel Pkg"
+
+	[ -f Mawrol-kernel-$VERSION.zip ] && echo "Removing Exist Pkg"
+	[ -f Mawrol-kernel-$VERSION.zip ] && rm Mawrol-kernel-$VERSION.zip 2>/dev/null
 
 	# Pack AnyKernel3
 	rm -rf kernelzip
 	mkdir kernelzip
 	echo "
-kernel.string=arter97 kernel $(cat version) @ xda-developers
+kernel.string=Mawrol kernel $(cat version) @ xda-developers
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
@@ -35,10 +37,11 @@ ramdisk_compression=gzip
 	cp -rp ~/android/anykernel/* kernelzip/
 	find arch/arm64/boot/dts -name '*.dtb' -exec cat {} + > kernelzip/dtb
 	cd kernelzip/
-	7z a -mx9 arter97-kernel-$VERSION-tmp.zip *
-	7z a -mx0 arter97-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
-	zipalign -v 4 arter97-kernel-$VERSION-tmp.zip ../arter97-kernel-$VERSION.zip
-	rm arter97-kernel-$VERSION-tmp.zip
+	7z a -mx9 Mawrol-kernel-$VERSION-tmp.zip *
+	7z a -mx0 Mawrol-kernel-$VERSION-tmp.zip ../arch/arm64/boot/Image.gz
+	zipalign -v 4 Mawrol-kernel-$VERSION-tmp.zip ../Mawrol-kernel-$VERSION.zip
+	rm Mawrol-kernel-$VERSION-tmp.zip
 	cd ..
-	ls -al arter97-kernel-$VERSION.zip
+	ls -al Mawrol-kernel-$VERSION.zip
 fi
+
